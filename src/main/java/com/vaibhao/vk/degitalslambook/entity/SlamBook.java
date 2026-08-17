56beb1b38f6fdec1a31ce362e81f41b16fda77fe
Vaibhao Kamble <vaibhaokamble@gmail.com>
2026-08-17 15:35:28 +0530

    change in the entity

--- src/main/java/com/vaibhao/vk/degitalslambook/entity/SlamBook.java
+++ src/main/java/com/vaibhao/vk/degitalslambook/entity/SlamBook.java
@@ -1,39 +1,49 @@
 package com.vaibhao.vk.degitalslambook.entity;
 
 import com.vaibhao.vk.degitalslambook.enums.Gender;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "slam_book")
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class SlamBook {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String fullName;
 
     private String nickName;
 
     private String profilePhotoUrl;
 
+    @Enumerated(EnumType.STRING)
     private LocalDate dateOfBirth;
 
     private Gender gender;
 
     private String favoriteColor;
 
-    private String hobbies;
+    private List<String> hobbies;
 
+    @Column(length = 500)
     private String aboutMe;
 
+    @Column(nullable = false)
     private int rating;
 
+    @Column(nullable = false)
     private Boolean bestFriend;
 
     private LocalDate friendshipDate;
@@ -46,12 +56,15 @@ public class SlamBook {
 
     private String songDedication;
 
-    private String memoryPhoto;
+    private String memoryPhotoUrl;
 
-    private String memoriesText;
+    @Column(length = 500)
+    private String memoryText;
 
+    @CreationTimestamp
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
