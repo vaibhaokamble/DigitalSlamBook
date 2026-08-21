bfe1eefe2d705ad826cf17327ad47bacd284c8bd
Vaibhao Kamble <vaibhaokamble@gmail.com>
2026-08-21 16:34:48 +0530

    fix cors on versel

--- src/main/java/com/vaibhao/vk/digitalslambook/config/WebConfig.java
+++ src/main/java/com/vaibhao/vk/digitalslambook/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081", "http://localhost:5173", "https://digital-slam-book-qlgfctdbs-vaibhao.vercel.app")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
