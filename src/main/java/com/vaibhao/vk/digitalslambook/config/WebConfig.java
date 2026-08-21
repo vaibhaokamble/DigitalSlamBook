30a410e44f16396d1e49c683172de2933262d54e
Vaibhao Kamble <vaibhaokamble@gmail.com>
2026-08-21 16:11:18 +0530

    add vercel url

--- src/main/java/com/vaibhao/vk/digitalslambook/config/WebConfig.java
+++ src/main/java/com/vaibhao/vk/digitalslambook/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081", "http://localhost:5173", "http://localhost:8082")
+                .allowedOrigins("http://localhost:8081", "http://localhost:5173", "https://digital-slam-book-qlgfctdbs-vaibhao.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
